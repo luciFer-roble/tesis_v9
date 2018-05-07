ffc06b4ba363236ef9373ae8f8ab6bdef70b9412
SebastiaN <odm_sd@hotmail.com>
2018-05-06 19:52:39 -0500

    configuracion js

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,17 +1,18 @@
 
 window._ = require('lodash');
+window.Popper = require('popper.js').default;
 
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
  * for JavaScript based Bootstrap features such as modals and tabs. This
  * code may be modified to fit the specific needs of your application.
- */
 
 try {
     window.$ = window.jQuery = require('jquery');
 
     require('bootstrap-sass');
 } catch (e) {}
+ */
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -22,6 +23,9 @@ try {
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+moment.locale('es');
+
 
 /**
  * Next we will register the CSRF Token as a common header with Axios so that

3fdab1638eda1a441824e5b61ecd2ed07d9d28dd
Jason <jtaly1717@gmail.com>
2018-07-17 01:55:11 -0500

    login v2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,7 +16,7 @@
         <div class="panel panel-info" >
             <div class="panel-heading">
                 <div class="panel-primary">INGRESAR</div>
-                <div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="#">Olvidaste tu contrasena?</a></div>
+                <div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="{{ route('password.request') }}">Olvidaste tu contrasena?</a></div>
             </div>
 
             <div style="padding-top:30px" class="panel-body" >
@@ -29,11 +29,23 @@
                     <div style="margin-bottom: 25px" class="input-group">
                         <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
                         <input id="email" type="email" class="form-control" name="email"  placeholder="correo electronico" value="{{ old('email') }}" required autofocus>
+                        @if ($errors->has('email'))
+                            <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                        @endif
                     </div>
                     </div>
+                    <div class="{{ $errors->has('password') ? ' has-error' : '' }}">
                     <div style="margin-bottom: 25px" class="input-group">
                         <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                        <input id="login-password" type="password" class="form-control" name="password" placeholder="password">
+                        <input id="password" type="password" class="form-control" name="password" placeholder="clave" required>
+                        @if ($errors->has('password'))
+                            <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                        @endif
+                    </div>
                     </div>
 
 
@@ -41,7 +53,7 @@
                     <div class="input-group">
                         <div class="checkbox">
                             <label>
-                                <input id="login-remember" type="checkbox" name="remember" value="1"> Remember me
+                                <input  type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Recordarme
                             </label>
                         </div>
                     </div>
@@ -49,22 +61,14 @@
 
                     <div style="margin-top:10px" class="form-group">
                         <!-- Button -->
-
-                        <div class="col-sm-12 controls">
-                            <a id="btn-login" href="#" class="btn btn-success">Login  </a>
-                            <a id="btn-fblogin" href="#" class="btn btn-primary">Login with Facebook</a>
-
-                        </div>
                     </div>
 
 
                     <div class="form-group">
                         <div class="col-md-12 control">
                             <div style="border-top: 1px solid#888; padding-top:15px; font-size:85%" >
-                                Don't have an account!
-                                <a href="#" onClick="$('#loginbox').hide(); $('#signupbox').show()">
-                                    Sign Up Here
-                                </a>
+                                <button type="submit"  class="btn btn-success">Login</button>
+
                             </div>
                         </div>
                     </div>
@@ -74,81 +78,6 @@
 
             </div>
         </div>
-    </div>
-    <div id="signupbox" style="display:none; margin-top:50px" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
-        <div class="panel panel-info">
-            <div class="panel-heading">
-                <div class="panel-title">Sign Up</div>
-                <div style="float:right; font-size: 85%; position: relative; top:-10px"><a id="signinlink" href="#" onclick="$('#signupbox').hide(); $('#loginbox').show()">Sign In</a></div>
-            </div>
-            <div class="panel-body" >
-                <form id="signupform" class="form-horizontal" role="form">
-
-                    <div id="signupalert" style="display:none" class="alert alert-danger">
-                        <p>Error:</p>
-                        <span></span>
-                    </div>
-
-
-
-                    <div class="form-group">
-                        <label for="email" class="col-md-3 control-label">Email</label>
-                        <div class="col-md-9">
-                            <input type="text" class="form-control" name="email" placeholder="Email Address">
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="firstname" class="col-md-3 control-label">First Name</label>
-                        <div class="col-md-9">
-                            <input type="text" class="form-control" name="firstname" placeholder="First Name">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="lastname" class="col-md-3 control-label">Last Name</label>
-                        <div class="col-md-9">
-                            <input type="text" class="form-control" name="lastname" placeholder="Last Name">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="password" class="col-md-3 control-label">Password</label>
-                        <div class="col-md-9">
-                            <input type="password" class="form-control" name="passwd" placeholder="Password">
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="icode" class="col-md-3 control-label">Invitation Code</label>
-                        <div class="col-md-9">
-                            <input type="text" class="form-control" name="icode" placeholder="">
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <!-- Button -->
-                        <div class="col-md-offset-3 col-md-9">
-                            <button id="btn-signup" type="button" class="btn btn-info"><i class="icon-hand-right"></i> &nbsp Sign Up</button>
-                            <span style="margin-left:8px;">or</span>
-                        </div>
-                    </div>
-
-                    <div style="border-top: 1px solid #999; padding-top:20px"  class="form-group">
-
-                        <div class="col-md-offset-3 col-md-9">
-                            <button id="btn-fbsignup" type="button" class="btn btn-primary"><i class="icon-facebook"></i>   Sign Up with Facebook</button>
-                        </div>
-
-                    </div>
-
-
-
-                </form>
-            </div>
-        </div>
-
-
-
-
     </div>
 </div>
 

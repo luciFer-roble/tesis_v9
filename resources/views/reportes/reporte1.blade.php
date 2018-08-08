46c4cc1ffa8d8db60e7cf4623d12a96de063f5db
Jason <jtaly1717@gmail.com>
2018-08-08 16:26:29 -0500

    reporte1 v2

--- resources/views/reportes/reporte1.blade.php
+++ resources/views/reportes/reporte1.blade.php
@@ -11,39 +11,39 @@
             <div class="card-header">
                 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center  ">
                     <div class="btn-toolbar mb-2 mb-md-0">
-                        <h1>PRACTICAS</h1></div>
+                        <h3>{{ $periodo->descripcionperiodoacademico }}</h3></div>
 
-                    <div class="btn-group mr-2">
+                    <div class="btn-group mr-2 float-right">
                         <input type="button" onClick="location.href = 'practicas/create'"
-                               class="btn btn-sm btn-outline-success" value="DESCARGAR"/>
+                               class="btn btn-sm btn-outline-danger" value="PDF"/>
+                        <input type="button" onClick="location.href = 'practicas/create'"
+                               class="btn btn-sm btn-outline-success" value="EXCEL"/>
                     </div>
 
                 </div>
                 `
             </div>
-            <div class="card-body" id="app">
+            <div class="card-body p-0 m-0" id="app">
 
                 <div class="table-responsive">
 
-                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <table class="table table-bordered p-0 m-0 border-0" id="dataTable" width="100%" style="table-layout: fixed;  display: table;">
                         <thead>
                             <tr>
-                                <th >Cedula</th>
-                                <th >Nombre</th>
-                                <th >Celular</th>
-                                <th >Correo</th>
-                                <th >Facultad</th>
-                                <th >Carrera</th>
-                                <th >Horas</th>
+                                <th  style="width:  12%" class="p-1 m-0">Identificacion</th>
+                                <th  style="width:  16%" class="p-1 m-0">Apellidos y Nombres</th>
+                                <th  style="width:  13%" class="p-1 m-0">Unidad Academica</th>
+                                <th  style="width:  25%" class="p-1 m-0">Carrera</th>
+                                <th  style="width:  13%" class="p-1 m-0">Celular</th>
+                                <th  style="width:  14%" class="p-1 m-0">Correo</th>
+                                <th  colspan="2"  style="width:  7.318912295584146%;" class="p-1 m-0">Horas</th>
                             </tr>
                         </thead>
-                        <tbody>
-                        @foreach($estudiantes as $estudiante)
-                            <tr is="estudiante-item" :estudiante="{{ $estudiante }}" carrera="{{ $estudiante->carrera->nombrecarrera }}"
-                            facultad="{{ $estudiante->carrera->escuela->facultad->nombrefacultad }}"></tr>
-                        @endforeach
-                        </tbody>
                     </table>
+                        @foreach($estudiantes as $estudiante)
+                            <estudiante-item  :estudiante="{{ $estudiante }}" carrera="{{ $estudiante->carrera->nombrecarrera }}"
+                                facultad="{{ $estudiante->carrera->escuela->facultad->nombrefacultad }}"></estudiante-item>
+                        @endforeach
                 </div>
             </div>
         </div>

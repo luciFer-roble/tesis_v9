d7d84d7ca003b4f32cb4427a18fefda960c9ccf0
luciFer-roble <39034533+luciFer-roble@users.noreply.github.com>
2018-08-15 00:30:01 -0500

    conflictos de css arreglados!

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,7 +6,6 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>Practicas Pre-Profesionales</title>
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <link rel="stylesheet" href="{{mix('/css/app.css')}}">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
 <!--

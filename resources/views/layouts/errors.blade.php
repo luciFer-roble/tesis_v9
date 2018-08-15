47ea7c74fbf296283e933ac402dfcbef05a210d3
Jason <jtaly1717@gmail.com>
2018-08-15 01:18:08 -0500

    color del error

--- resources/views/layouts/errors.blade.php
+++ resources/views/layouts/errors.blade.php
@@ -1,6 +1,6 @@
 @if(count($errors))
     <div class="form-group">
-        <div class="alert alert-danger">
+        <div class="alert alert-danger" style="background-color: #005cbf !important; border-color: #005cbf !important">
             <ul>
                 @foreach($errors->all() as $error)
                     <li>{{ $error  }}</li>

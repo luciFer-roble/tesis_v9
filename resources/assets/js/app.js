473f761df995671cac0b2450d327a3e0bde9baa2
Jason <jtaly1717@gmail.com>
2018-08-26 21:49:20 -0500

    empresas index v3
    como jode la fer

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -82,6 +82,7 @@ Vue.component('tutores-componente', require('./components/tutores-componente.vue
 Vue.component('tutores-nuevo', require('./components/tutores-nuevo.vue'));
 Vue.component('coordinadores-componente', require('./components/coordinadores-componente.vue'));
 Vue.component('coordinadores-nuevo', require('./components/coordinadores-nuevo.vue'));
+Vue.component('empresa-nuevo', require('./components/empresa-nuevo.vue'));
 const app = new Vue({
     el: '#app'
 });

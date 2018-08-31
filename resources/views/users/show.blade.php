4e0931562d0491922063a36ab100964f51bc9970
luciFer-roble <39034533+luciFer-roble@users.noreply.github.com>
2018-08-30 23:05:57 -0500

    Pefil de Usuario parte 1

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -9,57 +9,98 @@
 
 @section('content')
 
-        <div class="container-fluid">
+    <div class="container-fluid">
 
-            <div class="row">
-                <div class="col-md-6">
+        <div class="row">
+            <div class="col-md-6">
 
 
-                    <!-- Example DataTables Card-->
-                    <div class="card card-info">
-                        <div class="card-header">
-                        </div>
-                       {{Form::open( ['method'=>"PUT", 'url'=>array("/users", $user->id), 'files'=>true]) }}
+                <!-- Example DataTables Card-->
+                <div class="card ">
+                    @if(Auth::user()->hasRole('admin'))
+                           {{Form::open( ['method'=>"PUT", 'url'=>array("/users", $user->id), 'files'=>true]) }}
 
-                            {{ csrf_field() }}
-                                <div class="card-body">
-                                    <p>
-                                        <strong>Nombre:</strong> {{ $user->name }}<br>
-                                        <strong>Correo:</strong> {{ $user->email }}<br>
-                                    <div class="form-group">
-                                        <div class="image">
-                                            <img src="/uploads/avatars/{{$user->avatar}}"
-                                                 alt="User Image">
+                           {{ csrf_field() }}
+                           <div class="card-body">
+                               <table class="table table-bordered">
+                                  <th><strong>Nombre:</strong> </th>
+                                   <td>{{ $user->name }}</td>
+                                   <th><strong>Correo:</strong></th>
+                                   <td>{{ $user->email }}</td>
+                                   <td colspan="2">
+                                       <div class="form-group">
+                                           <div class="image">
+                                               <img src="/uploads/avatars/{{$user->avatar}}"
+                                                    alt="User Image">
 
-                                        </div>
-                                        <div class="input-group">
-                                                <div class="custom-file">
-                                                    <input type="file" class="" id="foto" name="foto">
-                                                    <label for="foto"></label>
-                                                </div>
+                                           </div>
+                                           <div class="input-group">
+                                               <div class="custom-file">
+                                                   <input type="file" class="" id="foto" name="foto">
+                                                   <label for="foto"></label>
+                                               </div>
 
-                                        </div>
+                                           </div>
 
+                                       </div>
+                                   </td>
+                               </table>
+                           </div>
+                    @else
+                        {{Form::open( ['method'=>"PUT", 'url'=>array("/users", $usuario->id), 'files'=>true]) }}
+
+                        {{ csrf_field() }}
+                        <div class="card-body">
+                            <div class="formgroup" width="100">
+                                <label for="nombre">Nombre:</label>
+                                <input type="text" class="form-control" id="nombre" name="nombre" value="{{ $usuario->nombre }}" disabled>
+                            </div>
+
+                            <div class="formgroup">
+                                <label for="apellido">Apellido:</label>
+                                <input type="text" class="form-control" id="apellido" name="apellido" value="{{ $usuario->apellido }}"disabled>
+                            </div>
+
+                            <div class="formgroup">
+                                <label for="celular">Celular:</label>
+                                <input type="text" class="form-control" id="celular" name="celular" value="{{ $usuario->celular}}">
+                            </div>
+
+                            <div class="formgroup">
+                                <label for="correo">Correo:</label>
+                                <input type="text" class="form-control" id="correo" name="correo" value="{{ $usuario->correo }}">
+                            </div>
+                            <div class="form-group">
+                                <div class="image">
+                                    <img src="/uploads/avatars/{{$usuario->avatar}}"
+                                         alt="User Image">
+
+                                </div>
+                                <div class="input-group">
+                                    <div class="custom-file">
+                                        <input type="file" class="" id="foto" name="foto">
+                                        <label for="foto"></label>
                                     </div>
 
-
-
-                                    </p>
                                 </div>
-                        <div class="card-footer">
-                            <div class="form-group">
-                                <button type="submit" class="btn btn-primary">Guardar</button>
 
                             </div>
                         </div>
+                    @endif
+                    <div class="card-footer">
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary">Guardar</button>
 
-
-                        </form>
+                        </div>
                     </div>
 
+
+                    </form>
                 </div>
+
             </div>
         </div>
+    </div>
 
 
 @stop

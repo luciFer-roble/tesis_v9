680d68588a8f8553129b7e3c7b40db67cc25017f
Jason <jtaly1717@gmail.com>
2018-10-01 16:17:11 -0500

    con certeridad

--- app/Http/Controllers/sshController.php
+++ app/Http/Controllers/sshController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class sshController extends Controller
 {
     public function pull(){
-        exec('cd /home/mansor/webapps/app_vinculacion/tesis_v9;git pull origin;');
+        return exec('cd /home/mansor/webapps/app_vinculacion/tesis_v9;git pull origin;');
 
     }
 }

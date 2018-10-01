4dae90f18f96d188844858fb2de9329c6ec3262f
Jason <jtaly1717@gmail.com>
2018-10-01 16:19:22 -0500

    ahora si a de ser

--- app/Http/Controllers/sshController.php
+++ app/Http/Controllers/sshController.php
@@ -7,7 +7,6 @@ use Illuminate\Http\Request;
 class sshController extends Controller
 {
     public function pull(){
-        return exec('cd /home/mansor/webapps/app_vinculacion/tesis_v9;git pull origin;');
-
+        return dd(exec('cd /home/mansor/webapps/app_vinculacion/tesis_v9;git pull origin;'));
     }
 }

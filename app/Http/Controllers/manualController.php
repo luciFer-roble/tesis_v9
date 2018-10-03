a888925191e9385d5aa1eca5d2e6fa35514026d8
Jason <jtaly1717@gmail.com>
2018-10-03 13:19:53 -0500

    manual en public

--- app/Http/Controllers/manualController.php
+++ app/Http/Controllers/manualController.php
@@ -9,6 +9,6 @@ class manualController extends Controller
     public function descargar()
     {
         $headers = ['Content-Type: application/zip','Content-Disposition: attachment; filename={$filename}'];
-        return response()->download(storage_path('app/public/').'manual.pdf ', 'ManualPPP.pdf', $headers );
+        return response()->download(public_path('uploads/manual.pdf'), 'ManualPPP.pdf', $headers );
     }
 }
